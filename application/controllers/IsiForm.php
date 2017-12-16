4eabbe06b99d499e9fb45954b064f57db82c47ec
Moh. Firman Qodry <firman.qodry@gmail.com>
2017-12-17 05:14:39 +0700

    update kemarin 17-12-2017

--- application/controllers/IsiForm.php
+++ application/controllers/IsiForm.php
@@ -45,65 +45,103 @@ class IsiForm extends CI_Controller {
 
 	function formpakarHandler() {
 		$this->load->library('form_validation');
-		// $rules = array(
-		// 	array(
-		// 		'field'	=> 'beratbadan',
-		// 		'label'	=> 'Berat Badan',
-		// 		'rules'	=> 'trim|required|xss_clean|decimal'
-		// 	),
-		// 	array(
-		// 		'field'	=> 'tinggibadan',
-		// 		'label'	=> 'Tinggi Badan',
-		// 		'rules'	=> 'trim|required|xss_clean|decimal'
-		// 	),
-		// 	array(
-		// 		'field'	=> 'usia',
-		// 		'label'	=> 'Usia',
-		// 		'rules'	=> 'trim|required|xss_clean|decimal'
-		// 	),
-		// 	array(
-		// 		'field'	=> 'jeniskelamin',
-		// 		'label'	=> 'Jenis Kelamin',
-		// 		'rules'	=> 'trim|required|xss_clean'
-		// 	),
-		// 	array(
-		// 		'field'	=> 'pekerjaan',
-		// 		'label'	=> 'Pekerjaan',
-		// 		'rules'	=> 'trim|required|xss_clean'
-		// 	)
-		// );
-		// $this->form_validation->set_rules($rules);
+		$rules = array(
+			array(
+				'field'	=> 'beratbadan',
+				'label'	=> 'Berat Badan',
+				'rules'	=> 'trim|required|xss_clean'
+			),
+			array(
+				'field'	=> 'tinggibadan',
+				'label'	=> 'Tinggi Badan',
+				'rules'	=> 'trim|required|xss_clean'
+			),
+			array(
+				'field'	=> 'usia',
+				'label'	=> 'Usia',
+				'rules'	=> 'trim|required|xss_clean'
+			),
+			array(
+				'field'	=> 'jeniskelamin',
+				'label'	=> 'Jenis Kelamin',
+				'rules'	=> 'trim|required|xss_clean'
+			),
+			array(
+				'field'	=> 'pekerjaan',
+				'label'	=> 'Pekerjaan',
+				'rules'	=> 'trim|required|xss_clean'
+			)
+		);
+		$this->form_validation->set_rules($rules);
 
-		// if($this->form_validation->run() == FALSE){
-		// 	if(isset($this->session->userdata['logged_in'])) {
-		// 		// set flashdata
-		// 		$flash_msg = array(
-		// 			'msg'	=> validation_errors(),
-		// 			'type'	=> "danger"
-		// 		);
-		// 		$this->session->set_flashdata('handler_msg',$flash_msg);
-		// 		redirect(base_url().'login');
-		// 	} else {
-		// 		// set flashdata
-		// 		$flash_msg = array(
-		// 			'msg'	=> validation_errors(),
-		// 			'type'	=> "danger"
-		// 		);
-		// 		$this->session->set_flashdata('handler_msg',$flash_msg);
+		if($this->form_validation->run() == FALSE){
+			if(isset($this->session->userdata['logged_in'])) {
+				// set flashdata
+				$flash_msg = array(
+					'msg'	=> validation_errors(),
+					'type'	=> "danger"
+				);
+				$this->session->set_flashdata('handler_msg',$flash_msg);
+				redirect(base_url().'login');
+			} else {
+				// set flashdata
+				$flash_msg = array(
+					'msg'	=> validation_errors(),
+					'type'	=> "danger"
+				);
+				$this->session->set_flashdata('handler_msg',$flash_msg);
 
-		// 		redirect(base_url().'isiform');
-		// 	}
-		// } else {
+				redirect(base_url().'isiform');
+			}
+		} else {
+			$data['title'] = "Pakar Diet - Dashboard";
+			$data['user_id'] = $this->session->userdata['logged_in']['user_id'];
+			$data['username'] = $this->session->userdata['logged_in']['username'];
+			$data['first_name'] = $this->session->userdata['logged_in']['first_name'];
+			$data['last_name'] = $this->session->userdata['logged_in']['last_name'];
+			$user_role = $this->session->userdata['logged_in']['user_role'];
+
+			if(!empty($user_role) && $user_role == 'ROLE_ADMIN') {
+				$data['admin_zone'] = true;
+			} else {
+				$data['admin_zone'] = false;
+			}
+
 			$beratbadan = $this->input->post('beratbadan');
 			$tinggibadan = $this->input->post('tinggibadan');
 			$usia = $this->input->post('usia');
 			$jeniskelamin = $this->input->post('jeniskelamin');
 			$pekerjaan = $this->input->post('pekerjaan');
 
-			$bbideal = ( (int)$tinggibadan - 100 ) * (10/100);
+			$data['form_pakar'] = base_url() . "isiform/formpakarHandler";
+			$data['jobs'] = $this->Default_md->getAll('m_pekerjaan');
 
-			echo "bb ideal kamu = ".$bbideal." kg";
-		// }
+			// HITUNG BB IDEAL
+			(float)$bbideal = ( (int)$tinggibadan - 100 ) - (10/100);
+			$data['bbideal'] = $bbideal;
+
+			// HITUNG IMT
+			(float)$imt = (int)$beratbadan / pow(((int)$tinggibadan/100), 2);
+			$data['imt'] = $imt;
+
+			// HITUNG KEBUTUHAN KALORI
+			switch($jeniskelamin){
+				case 'M':
+					(float)$kebutuhan_kalori = 65 + (13.7 * (float)$bbideal) + (5 * (int)$tinggibadan) - (6.8 * (int)$usia);
+					break;
+				case 'F':
+					(float)$kebutuhan_kalori = 655 + (9.6 * (float)$bbideal) + (1.8 * (int)$tinggibadan) - (4.7 * (int)$usia);
+					break;
+			}
+			$data['kebutuhan_kalori'] = $kebutuhan_kalori;
+
+			echo "BB Ideal = ".$bbideal."<br/>";
+			echo "IMT = ".$imt."<br/>";
+			echo "Kebutuhan Kalori = ".$kebutuhan_kalori;
+
+			$this->load->view('template/header',$data);
+			$this->load->view('hitungkalori',$data);
+		}
 	}
 
 
---
+++ application/views/hitungkalori.php
@@ -0,0 +1,87 @@
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="row">
+              <div class="col-md-6 col-md-offset-3">
+                <?php 
+                    $flash_message = $this->session->flashdata('handler_msg');
+                    if( ! empty($flash_message) ) {
+                    echo '<p class="alert alert-'. $flash_message['type'] .'" id="flash_message"><b>'. $flash_message['msg'] .'</b></p>';
+                    }
+                ?>
+                <h3>Result Data</h3><br/>
+                <div class="table-responsive">
+                    <table class="table table-condensed table-bordered">
+                        <thead>
+                            <tr>
+                                <th colspan="2" style="text-align: center;">RESULT</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><strong>BB Ideal:</strong></td>
+                                <td><strong></strong></td>
+
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            &copy; <?php echo date('Y'); ?> Sistem Pakar Diet
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="assets/vendors/js/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="assets/vendors/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="assets/vendors/js/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="assets/vendors/js/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="assets/vendors/js/Chart.min.js"></script>
+    <!-- jQuery Sparklines -->
+    <script src="assets/vendors/js/jquery.sparkline.min.js"></script>
+    <!-- morris.js -->
+    <script src="assets/vendors/js/raphael.min.js"></script>
+    <script src="assets/vendors/js/morris.min.js"></script>
+    <!-- gauge.js -->
+    <script src="assets/vendors/js/gauge.min.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="assets/vendors/js/bootstrap-progressbar.min.js"></script>
+    <!-- Skycons -->
+    <script src="assets/vendors/js/skycons.js"></script>
+    <!-- Flot -->
+    <script src="assets/vendors/js/jquery.flot.js"></script>
+    <script src="assets/vendors/js/jquery.flot.pie.js"></script>
+    <script src="assets/vendors/js/jquery.flot.time.js"></script>
+    <script src="assets/vendors/js/jquery.flot.stack.js"></script>
+    <script src="../vendors/Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="assets/vendors/js/jquery.flot.orderBars.js"></script>
+    <script src="assets/vendors/js/jquery.flot.spline.min.js"></script>
+    <script src="assets/vendors/js/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="assets/vendors/js/date.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="assets/vendors/js/moment.min.js"></script>
+    <script src="assets/vendors/js/daterangepicker.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="assets/js/custom.min.js"></script>
+
+  </body>
+</html>
