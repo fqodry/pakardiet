e734ba8c976e5964e9ebd7713c901116b43661a8
Moh. Firman Qodry <firman.qodry@gmail.com>
2017-12-17 14:40:43 +0700

    update header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -67,7 +67,8 @@
                     </ul>
                   </li> -->
                   <li><a href=""><i class="fa fa-home"></i> Home</a></li>
-                  <li><a href="isiform"><i class="fa fa-wpforms"></i> Isi Form <span class="label label-success pull-right">New</span></a></li>
+                  <li><a href="myprofile"><i class="fa fa-user"></i> My Profile</a></li>
+                  <li><a href="isiform"><i class="fa fa-wpforms"></i> Hitung Kebutuhan Kalori </a></li>
                   <?php if(!empty($admin_zone) && $admin_zone === true){ ?>
                   <li><a><i class="fa fa-database"></i> Master Data <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
