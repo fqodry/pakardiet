03d6559285278bc00f3089c2a830b0901c9ad625
Moh. Firman Qodry <firman.qodry@gmail.com>
2017-12-19 22:22:37 +0700

    update histformpakar model

--- application/models/Hist_formpakar_md.php
+++ application/models/Hist_formpakar_md.php
@@ -16,25 +16,4 @@ class Hist_formpakar_md extends CI_Model {
 			return false;
 		}
 	}
-
-	function getHistFormPakar($user_id) {
-		$query = "
-			SELECT
-				a.histform_id AS histform_id,
-				a.berat_badan AS bb,
-				b.bb_ideal AS bbideal,
-				a.created_by AS createdBy,
-				a.created_date AS createdDate
-			FROM
-				tb_hist_formpakar a
-			JOIN tb_hist_formpakar_result b ON b.histform_id = a.histform_id
-			WHERE
-				a.user_id = '".$user_id."'";
-		$result = $this->db->query($query);
-		if($result->num_rows() == 1){
-			return $result->row();
-		}else{
-			return false;
-		}
-	}
 }
